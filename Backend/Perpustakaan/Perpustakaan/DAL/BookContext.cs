73a19a283f3d69d7e70034cfca665e14b1f0081a
rantipuspitasari <rantipuspitasari12@gmail.com>
2016-09-26 14:10:49 +0700

    update bookContext

--- Backend/Perpustakaan/Perpustakaan/DAL/BookContext.cs
+++ Backend/Perpustakaan/Perpustakaan/DAL/BookContext.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Data;
 using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
 using Perpustakaan.Models;
 
 namespace Perpustakaan.DAL
