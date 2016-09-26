6bbb53b25c718ecbbf04992a75b4ba399d085f91
eefsaefullah <eefsaefullah121@gmail.com>
2016-09-26 10:43:21 +0700

    add member class

---
+++ Backend/Perpustakaan/Perpustakaan/Models/Member.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Perpustakaan.Models
+{
+    public class Member
+    {
+        public int ID { get; set; }
+        public char MemberID { get; set; }
+        public string Nama { get; set; }
+        public string JenisKelamin { get; set; }
+        public string Alamat { get; set; }
+        public char NoTelp { get; set; }
+    }
+}
